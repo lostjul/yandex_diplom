78b663dc3dd9d58ce923292211a27ed7672de4d0
Jule <lostjul14@yandex,ru>
2024-08-18 18:31:53 +0500

    Добавлен скриншот запуска теста

--- get_order_by_track_after_create.py
+++ get_order_by_track_after_create.py
@@ -1,6 +1,7 @@
 import sender_stand_request
 
 
+# Пахомова Юлия Петровна, 20 когорта. Финальный проект. Инженер по тестированию плюс
 def test_assert_status_success():
     order_track = sender_stand_request.post_order_create().json()["track"]
     request = sender_stand_request.get_order_by_track(order_track)
